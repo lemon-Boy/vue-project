c7af2c579d92e7d20e4fd60ab776d3e24a251024
Wu <wulh@microport.com>
2020-05-09 17:35:14 +0800

    some changes

--- src/music/music.vue
+++ src/music/music.vue
@@ -2,22 +2,22 @@
   <div class="box">
     <input type="text"
            placeholder="Search" />
-    <div class="title flex around">
+    <div class="title flex around mt">
       <button type="button"></button>
       <p></p>
       <button type="button"></button>
     </div>
     <div v-for="(index,item) in info"
          :key="item.id"
-         class="voicebox flex">
-      <!-- <p>{{info[item].name}}</p> -->
-      <!-- <img :src="info[item].album.picUrl"
-           width="40px"
-           height="40px" />
+         class="voicebox center">
+      <p class="songname likefont c-white largefont">{{info[item].name}}</p>
+      <p class="author likefont mediumfont">{{info[item].artists[0].name}}</p>
+      <img class="songphoto"
+           :src="info[item].album.picUrl" />
       <audio :src='info[item].url'
              controls='true'>
         您的浏览器不支持 audio 标签。
-      </audio> -->
+      </audio>
     </div>
   </div>
 </template>
@@ -36,7 +36,7 @@ export default {
     })
       .then(response => {
         this.info = response.data.data.list.splice(0, 1)
-        console.log(response.data.data)
+        console.log(this.info)
       })
       .catch(error => { console.log(error, 'error', this.errored = true) }) // 失败的返回
       .finally(() => { this.loading = false })
@@ -46,7 +46,8 @@ export default {
 <style lang="less" scoped>
 .box {
   height: 100%;
-  background-color: #dbebf1a1;
+  background-color: #454859;
+  color: white;
 }
 input {
   outline: none;
@@ -59,9 +60,9 @@ input {
   background: #ffffff;
   box-shadow: inset 2px 2px 6px #babecc;
   transition: all 0.2s ease-in-out;
-}
-input:focus {
-  box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px white;
+  &:focus {
+    box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px white;
+  }
 }
 button {
   width: 35px;
@@ -69,15 +70,35 @@ button {
   border: none;
   outline: none;
   border-radius: 20px;
-  background: linear-gradient(45deg, #ddfbff, #bad4e3);
-  box-shadow: 10px 10px 25px #99aeba, -10px -10px 25px #ffffff;
+  background: linear-gradient(315deg, #454859, #454859);
+  box-shadow: -3px -3px 22px #1c1d24, 3px 3px 22px #6e738e;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
   &:hover {
-    box-shadow: 2px 2px 6px #cee5ef, 2px 2px 6px #dcdcdc;
+    background: #454859;
+    box-shadow: -2px -2px 10px #1c1d24, 2px 2px 10px #6e738e;
   }
   &:active {
-    box-shadow: 2px 2px 15px #e8f2f6, 2px 2px 15px #c7e3ed;
+    background: linear-gradient(315deg, #3e4150, #4a4d5f);
+    box-shadow: inset 1px 1px 5px #1c1d24, inset 1px 1px 5px #6e738ecc;
+  }
+}
+.songname {
+  margin: 5vh 0;
+}
+.songphoto {
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  transition: 5s;
+  animation: songphoto 20s linear infinite; // 指定旋转速度及动画
+}
+@keyframes songphoto {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
   }
 }
 </style>
