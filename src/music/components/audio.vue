21f5238cc25ca468186e20e6fd790570c450e030
Wu <wulh@microport.com>
2020-05-09 17:35:14 +0800

    some changes

---
+++ src/music/components/audio.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <audio :src='songval.url'
+           controls='true'>
+      您的浏览器不支持 audio 标签。
+    </audio>
+  </div>
+</template>
+<script>
+export default {
+  name: 'newAudio',
+  props: ['songval']
+}
+</script>
+<style lang="less" scoped>
+</style>
